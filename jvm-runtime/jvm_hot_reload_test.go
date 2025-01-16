3d851a4180f301c2f441ef502ab373a0b88ec413
Wes <wesbillman@users.noreply.github.com>
2025-01-15 18:40:43 -0700

    fix: integration tests and update docs

--- jvm-runtime/jvm_hot_reload_test.go
+++ jvm-runtime/jvm_hot_reload_test.go
@@ -29,18 +29,18 @@ func TestLifecycleJVM(t *testing.T) {
 			assert.Equal(t, 1, len(status.Deployments))
 			deployment = status.Deployments[0].Key
 		}),
-		in.Call("echo", "echo", "Bob", func(t testing.TB, response string) {
+		in.Call("echo", "hello", "Bob", func(t testing.TB, response string) {
 			assert.Equal(t, "Hello, Bob!", response)
 		}),
 		// Now test hot reload
 		in.IfLanguage("java", in.EditFile("echo", func(content []byte) []byte {
 			return []byte(strings.ReplaceAll(string(content), "Hello", "Bye"))
-		}, "src/main/java/ftl/echo/EchoVerb.java")),
+		}, "src/main/java/ftl/echo/Echo.java")),
 		in.IfLanguage("kotlin", in.EditFile("echo", func(content []byte) []byte {
 			return []byte(strings.ReplaceAll(string(content), "Hello", "Bye"))
-		}, "src/main/kotlin/ftl/echo/EchoVerb.kt")),
+		}, "src/main/kotlin/ftl/echo/Echo.kt")),
 		in.Sleep(time.Second*2), // Annoyingly quarkus limits to one restart check every 2s, which is fine for normal dev, but a pain for these tests
-		in.Call("echo", "echo", "Bob", func(t testing.TB, response string) {
+		in.Call("echo", "hello", "Bob", func(t testing.TB, response string) {
 			assert.Equal(t, "Bye, Bob!", response)
 		}),
 		in.VerifyControllerStatus(func(ctx context.Context, t testing.TB, status *ftlv1.StatusResponse) {
@@ -52,11 +52,11 @@ func TestLifecycleJVM(t *testing.T) {
 		// Structural change should result in a new deployment
 		in.IfLanguage("java", in.EditFile("echo", func(content []byte) []byte {
 			return []byte(strings.ReplaceAll(string(content), "@Export", ""))
-		}, "src/main/java/ftl/echo/EchoVerb.java")),
+		}, "src/main/java/ftl/echo/Echo.java")),
 		in.IfLanguage("kotlin", in.EditFile("echo", func(content []byte) []byte {
 			return []byte(strings.ReplaceAll(string(content), "@Export", ""))
-		}, "src/main/kotlin/ftl/echo/EchoVerb.kt")),
-		in.Call("echo", "echo", "Bob", func(t testing.TB, response string) {
+		}, "src/main/kotlin/ftl/echo/Echo.kt")),
+		in.Call("echo", "hello", "Bob", func(t testing.TB, response string) {
 			assert.Equal(t, "Bye, Bob!", response)
 		}),
 		in.VerifyControllerStatus(func(ctx context.Context, t testing.TB, status *ftlv1.StatusResponse) {
