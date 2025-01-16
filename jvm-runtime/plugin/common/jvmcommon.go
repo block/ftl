854f021b5cf2a11ecfb32255f6d437f5db67c117
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-01-16 12:33:51 +1100

    fix: force JVM code gen on stub sync

--- jvm-runtime/plugin/common/jvmcommon.go
+++ jvm-runtime/plugin/common/jvmcommon.go
@@ -924,6 +924,21 @@ func (s *Service) writeGenericSchemaFiles(ctx context.Context, v *schema.Schema,
 			return fmt.Errorf("failed to write schema file for module %s %w", mod.Name, err)
 		}
 	}
-
+	genCommand := ""
+	if config.LanguageConfig["build-tool"] == JavaBuildToolMaven {
+		genCommand = "mvn quarkus:generate-code"
+	} else {
+		genCommand = "gradle quarkusGenerateCode"
+	}
+	logger.Infof("Using generate command '%s'", genCommand)
+	command := exec.Command(ctx, log.Debug, config.Dir, "bash", "-c", genCommand)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	err = command.RunBuffered(ctx)
+	if err != nil {
+		logger.Errorf(err, "Generate process exited with error")
+	} else {
+		logger.Infof("Generate process exited")
+	}
 	return nil
 }
