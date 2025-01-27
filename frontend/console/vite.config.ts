9c0f065046f34f6a039c632f031c0dd8b4280906
Wes <wesbillman@users.noreply.github.com>
2025-01-27 11:03:34 -0700

    chore(rollup): update rollup config to fix warnings
    - add new `build:analyze` script to console for rollup visualizations

--- frontend/console/vite.config.ts
+++ frontend/console/vite.config.ts
@@ -1,8 +1,18 @@
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.ANALYZE === 'true' &&
+      visualizer({
+        open: true, // Automatically open the visualization in your browser
+        gzipSize: true, // Show gzipped sizes
+        brotliSize: true, // Show brotli sizes
+        filename: 'dist/stats.html', // Output file
+      }),
+  ].filter(Boolean),
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
@@ -11,30 +21,37 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
-          'vendor-ui': ['@headlessui/react', '@tanstack/react-query'],
-          'vendor-json-schema-faker': ['json-schema-faker'],
-          'vendor-json-schema-library': ['json-schema-library'],
-          'vendor-yaml': ['yaml'],
-          'vendor-fuse': ['fuse.js'],
-          'vendor-codemirror': [
-            '@codemirror/autocomplete',
-            '@codemirror/commands',
-            '@codemirror/lang-json',
-            '@codemirror/language',
-            '@codemirror/lint',
-            '@codemirror/state',
-            '@codemirror/view',
-          ],
-          'vendor-protos': [
-            './src/protos/xyz/block/ftl/console/v1/console_pb',
-            './src/protos/xyz/block/ftl/console/v1/console_connect',
-            './src/protos/xyz/block/ftl/schema/v1/schema_pb',
-            './src/protos/xyz/block/ftl/timeline/v1/event_pb',
-            './src/protos/xyz/block/ftl/timeline/v1/timeline_pb',
-            './src/protos/xyz/block/ftl/timeline/v1/timeline_connect',
-          ],
+        manualChunks(id) {
+          if (id.includes('node_modules/lodash')) {
+            return 'vendor-lodash'
+          }
+          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router-dom')) {
+            return 'vendor-react'
+          }
+          if (id.includes('node_modules/@headlessui/react') || id.includes('node_modules/@tanstack/react-query')) {
+            return 'vendor-ui'
+          }
+          if (id.includes('node_modules/json-schema-faker')) {
+            return 'vendor-json-schema-faker'
+          }
+          if (id.includes('node_modules/json-schema-library')) {
+            return 'vendor-json-schema-library'
+          }
+          if (id.includes('node_modules/yaml')) {
+            return 'vendor-yaml'
+          }
+          if (id.includes('node_modules/fuse.js')) {
+            return 'vendor-fuse'
+          }
+          if (id.includes('node_modules/reactflow') || id.includes('node_modules/@reactflow')) {
+            return 'vendor-reactflow'
+          }
+          if (id.includes('node_modules/@codemirror')) {
+            return 'vendor-codemirror'
+          }
+          if (id.includes('/protos/xyz/block/ftl/')) {
+            return 'vendor-protos'
+          }
         },
       },
     },
