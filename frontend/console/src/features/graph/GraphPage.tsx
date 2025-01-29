be65c3445294f9cb5b24368f81539e70a563176f
Wes <wesbillman@users.noreply.github.com>
2025-01-29 15:21:53 -0700

    fix: module page now shows graph view (#4231)
    ![Screenshot 2025-01-29 at 3 08
    00 PM](https://github.com/user-attachments/assets/0757943f-1fc1-4270-a96b-dc79f35218a6)
    ![Screenshot 2025-01-29 at 3 08
    04 PM](https://github.com/user-attachments/assets/0a9322a4-4b7c-4337-8d1d-9894b525110b)

--- frontend/console/src/features/graph/GraphPage.tsx
+++ frontend/console/src/features/graph/GraphPage.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react'
-import { Config, Data, Database, Enum, Module, Secret, Topic, Verb } from '../../protos/xyz/block/ftl/console/v1/console_pb'
-import type { ExpandablePanelProps } from '../../shared/components/ExpandablePanel'
 import { Loader } from '../../shared/components/Loader'
 import { ResizablePanels } from '../../shared/components/ResizablePanels'
-import { configPanels } from '../modules/decls/config/ConfigRightPanels'
-import { dataPanels } from '../modules/decls/data/DataRightPanels'
-import { databasePanels } from '../modules/decls/database/DatabaseRightPanels'
-import { enumPanels } from '../modules/decls/enum/EnumRightPanels'
-import { secretPanels } from '../modules/decls/secret/SecretRightPanels'
-import { topicPanels } from '../modules/decls/topic/TopicRightPanels'
-import { verbPanels } from '../modules/decls/verb/VerbRightPanel'
 import { useModules } from '../modules/hooks/use-modules'
 import { Timeline } from '../timeline/Timeline'
 import { GraphPane } from './GraphPane'
-import { modulePanels } from './ModulePanels'
 import { headerForNode } from './RightPanelHeader'
-import type { FTLNode } from './graph-utils'
+import { type FTLNode, panelsForNode } from './graph-utils'
 
 export const GraphPage = () => {
   const modules = useModules()
@@ -45,38 +35,4 @@ export const GraphPage = () => {
       />
     </div>
   )
-}
-
-const panelsForNode = (node: FTLNode | null, moduleName: string | null) => {
-  if (node instanceof Module) {
-    return modulePanels(node)
-  }
-
-  // If no module name is provided, we can't show the panels
-  if (!moduleName) {
-    return [] as ExpandablePanelProps[]
-  }
-
-  if (node instanceof Config) {
-    return configPanels(moduleName, node, false)
-  }
-  if (node instanceof Secret) {
-    return secretPanels(moduleName, node, false)
-  }
-  if (node instanceof Database) {
-    return databasePanels(moduleName, node, false)
-  }
-  if (node instanceof Enum) {
-    return enumPanels(moduleName, node, false)
-  }
-  if (node instanceof Data) {
-    return dataPanels(moduleName, node, false)
-  }
-  if (node instanceof Topic) {
-    return topicPanels(moduleName, node, false)
-  }
-  if (node instanceof Verb) {
-    return verbPanels(moduleName, node, false)
-  }
-  return [] as ExpandablePanelProps[]
 }
