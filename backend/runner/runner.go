af81492344449bcf9f580b304c9c64420e0157b2
Matt Toohey <contact@matttoohey.com>
2025-01-30 09:57:20 +1100

    fix: silence unwanted runner terminated errors

--- backend/runner/runner.go
+++ backend/runner/runner.go
@@ -79,7 +79,7 @@ type Config struct {
 
 func Start(ctx context.Context, config Config, storage *artefacts.OCIArtefactService) error {
 	ctx, doneFunc := context.WithCancelCause(ctx)
-	defer doneFunc(fmt.Errorf("runner terminated"))
+	defer doneFunc(context.Canceled)
 	hostname, err := os.Hostname()
 	if err != nil {
 		observability.Runner.StartupFailed(ctx)
