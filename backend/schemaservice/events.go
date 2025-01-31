39e7c01c5ccc4af768ef74dfab42a283b2f5adb0
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-01-31 13:54:36 +1100

    hack

--- backend/schemaservice/events.go
+++ backend/schemaservice/events.go
@@ -235,7 +235,12 @@ func handleChangesetCommittedEvent(ctx context.Context, t SchemaState, e *schema
 	for _, depName := range changeset.Deployments {
 		logger.Debugf("activating deployment %s", t.deployments[depName].GetRuntime().GetDeployment().Endpoint)
 		dep := t.deployments[depName]
+		old, ok := t.activeDeployments[dep.Name]
 		t.activeDeployments[dep.Name] = depName
+		if ok {
+			//TODO: huge hack, we need proper cleanup
+			delete(t.deployments, old)
+		}
 	}
 	return nil
 }
