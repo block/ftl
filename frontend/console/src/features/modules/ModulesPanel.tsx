90a2fc4ce2d97686aecaff05449d23b82db4e331
Wes <wesbillman@users.noreply.github.com>
2025-01-30 17:40:40 -0700

    fix: flickering on modules and graph pages (#4251)

--- frontend/console/src/features/modules/ModulesPanel.tsx
+++ frontend/console/src/features/modules/ModulesPanel.tsx
@@ -3,18 +3,22 @@ import { AttributeBadge } from '../../shared/components/AttributeBadge'
 import { List } from '../../shared/components/List'
 import { classNames } from '../../shared/utils'
 import { deploymentTextColor } from '../deployments/deployment.utils'
-import { useModules } from './hooks/use-modules'
+import { useStreamModules } from './hooks/use-stream-modules'
+import { sortModules } from './module.utils'
 
 export const ModulesPanel = () => {
-  const modules = useModules()
+  const { data } = useStreamModules()
+  const modules = sortModules(data?.modules ?? [])
 
   const moduleHref = (module: Module) => `/modules/${module.name}`
+  const moduleKey = (module: Module) => module.name
 
   return (
     <div className='p-2'>
       <List
-        items={modules.data?.modules ?? []}
+        items={modules}
         href={moduleHref}
+        keyExtractor={moduleKey}
         renderItem={(module) => (
           <div className='flex w-full' data-module-row={module.name}>
             <div className='flex gap-x-4 items-center w-1/2'>
