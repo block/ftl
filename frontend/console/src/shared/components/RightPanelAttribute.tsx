1f0d894157231725818862a8b007fbb54d3cb96a
Wes <wesbillman@users.noreply.github.com>
2025-02-03 14:39:26 -0700

    chore: only show scrollbar when needed (#4274)
    Will help with #4187

--- frontend/console/src/shared/components/RightPanelAttribute.tsx
+++ frontend/console/src/shared/components/RightPanelAttribute.tsx
@@ -3,7 +3,7 @@ export const RightPanelAttribute = ({ name, value }: { name?: string; value?: st
     <div className='flex justify-between space-x-2 items-center text-sm'>
       <span className='text-gray-500 dark:text-gray-400'>{name}</span>
       <span className='flex-1 min-w-0 text-right' title={value}>
-        <pre className='whitespace-pre-wrap overflow-x-scroll'>{value}</pre>
+        <pre className='whitespace-pre-wrap overflow-auto'>{value}</pre>
       </span>
     </div>
   )
