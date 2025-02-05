f4644a1487d566dc49e182a5f3ff26b0609a13e2
Wes <wesbillman@users.noreply.github.com>
2025-02-05 10:29:35 -0700

    chore: improve CI times for console e2e (#4318)
    Some changes/optimizations for the console e2e tests
    
    - Add more logging to report on module deployment time when starting
    `ftl dev`
      ```
      All modules ready after 96.698s
      Total startup time: 101.976s
      ```
    - Increase number of workers for playwright to `2` (previous was `1` on
    CI)
    - Increase number of cores for `ftl dev` to `2` on CI

--- frontend/console/playwright.config.ts
+++ frontend/console/playwright.config.ts
@@ -12,8 +12,8 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only. */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Use 2 workers in CI, auto-detect cores in development */
+  workers: process.env.CI ? 2 : undefined,
   /* With additional example modules, it can take a bit of time for everything to start up. */
   timeout: 90 * 1000,
   reporter: 'html',
@@ -28,19 +28,17 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
   ],
   webServer: {
-    command: 'ftl dev',
+    command: process.env.CI ? 'ftl dev -j 2' : 'ftl dev',
     url: 'http://localhost:8899',
     reuseExistingServer: !process.env.CI,
     /* If the test ends up needing to pull the postgres docker image, this can take a while. Give it a few minutes. */
