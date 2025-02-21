29f59269507881a4b09fd63b9ea0982d6f79866f
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-02-22 07:48:32 +1100

    fix: log startup errors

--- jvm-runtime/ftl-runtime/common/runtime/src/main/java/xyz/block/ftl/runtime/HotReloadSetup.java
+++ jvm-runtime/ftl-runtime/common/runtime/src/main/java/xyz/block/ftl/runtime/HotReloadSetup.java
@@ -1,16 +1,24 @@
 package xyz.block.ftl.runtime;
 
+import org.jboss.logging.Logger;
+
 import io.quarkus.dev.spi.HotReplacementContext;
 import io.quarkus.dev.spi.HotReplacementSetup;
 
 public class HotReloadSetup implements HotReplacementSetup {
+
+    private static final Logger LOG = Logger.getLogger(HotReloadSetup.class);
+    HotReplacementContext context;
+
     @Override
     public void setupHotDeployment(HotReplacementContext context) {
-
+        this.context = context;
     }
 
     @Override
     public void handleFailedInitialStart() {
+        LOG.errorf(context.getDeploymentProblem(), "Failed to start the FTL app, exiting JVM");
+
         // We need to exit the JVM if the app fails to start the first time
         System.exit(1);
     }
