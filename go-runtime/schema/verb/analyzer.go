4cdaccd86cd464df8a5eb8d58f72ced9d6f7733c
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-02-24 16:43:29 +1100

    fix: improve error message (#4588)
    fixes: #4572

--- go-runtime/schema/verb/analyzer.go
+++ go-runtime/schema/verb/analyzer.go
@@ -228,7 +228,7 @@ func checkSignature(
 		return optional.None[*types.Var](), optional.None[*types.Var]()
 	}
 	if !unicode.IsUpper(rune(node.Name.Name[0])) {
-		common.Errorf(pass, node, "verb name must be exported")
+		common.Errorf(pass, node, "verb function name must be exported (i.e. %s instead of %s)", strcase.ToUpperCamel(node.Name.Name), node.Name.Name)
 		return optional.None[*types.Var](), optional.None[*types.Var]()
 	}
 
