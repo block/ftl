0110ab5df64e042ca03fc16e3713a31d4b6da6f0
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-02-25 14:27:04 +1100

    foo

--- jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/TypeProcessor.java
+++ jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/TypeProcessor.java
@@ -1,20 +1,21 @@
 package xyz.block.ftl.deployment;
 
-import io.quarkus.deployment.annotations.BuildProducer;
-import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import java.util.Collection;
+import java.util.List;
+
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.ClassType;
 
-import java.util.Collection;
-import java.util.List;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 
 public class TypeProcessor {
 
     @BuildStep
     public void handleExportedTypes(CombinedIndexBuildItem index,
-                                    BuildProducer<SchemaContributorBuildItem> schemaContributorBuildItemBuildProducer,
-                                    List<TypeAliasBuildItem> typeAliasBuildItems // included to force typealias processing before this
+            BuildProducer<SchemaContributorBuildItem> schemaContributorBuildItemBuildProducer,
+            List<TypeAliasBuildItem> typeAliasBuildItems // included to force typealias processing before this
     ) {
         Collection<AnnotationInstance> exports = index.getIndex().getAnnotations(FTLDotNames.DATA);
         for (var an : exports) {
@@ -22,7 +23,8 @@ public class TypeProcessor {
                 continue;
             }
             schemaContributorBuildItemBuildProducer.produce(new SchemaContributorBuildItem(moduleBuilder -> moduleBuilder
-                    .buildType(ClassType.create(an.target().asClass().name()), an.target().hasAnnotation(FTLDotNames.EXPORT), an.target())));
+                    .buildType(ClassType.create(an.target().asClass().name()), an.target().hasAnnotation(FTLDotNames.EXPORT),
+                            an.target())));
         }
     }
 }
