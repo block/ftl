b74f58aaed40221311e7ed7a4ca75cf1f49089a9
Wes <wesbillman@users.noreply.github.com>
2025-02-27 10:16:31 -0700

    fix: remove duplicate schema panel (#4711)
    Fixes #4710
    
    ![Screenshot 2025-02-27 at 9 37
    14 AM](https://github.com/user-attachments/assets/44b7f50c-8d55-41b9-b74a-d5f66ddf607f)

--- frontend/console/src/features/graph/ModulePanels.tsx
+++ frontend/console/src/features/graph/ModulePanels.tsx
@@ -1,16 +1,9 @@
 import type { Module } from '../../protos/xyz/block/ftl/console/v1/console_pb'
 import type { ExpandablePanelProps } from '../../shared/components/ExpandablePanel'
 import { DeclDefaultPanels } from '../modules/decls/DeclDefaultPanels'
-import { Schema } from '../modules/schema/Schema'
 
 export const modulePanels = (module: Module): ExpandablePanelProps[] => {
   const panels = []
-
-  panels.push({
-    title: 'Schema',
-    expanded: true,
-    children: <Schema schema={module.schema} moduleName={module.name} />,
-  })
 
   panels.push(...DeclDefaultPanels(module.name, module.schema))
 
