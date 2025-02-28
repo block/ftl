0b42acede6856f48622e32b56084a26acce191bf
Alec Thomas <aat@block.xyz>
2025-02-28 17:25:52 +1100

    feat: add "ftl schema ebnf" (#4730)
    ```
    ~/dev/ftl $ ftl schema ebnf
    Schema = Module* .
    Module = <comment>* "builtin"? "module" <ident> "{" Metadata* Decl* "}" .
    Metadata = MetadataCalls | MetadataConfig | MetadataIngress | MetadataCronJob | MetadataDatabases | MetadataAlias | MetadataRetry | MetadataSecrets | MetadataSubscriber | MetadataTypeMap | MetadataEncoding | MetadataPublisher | MetadataSQLMigration | MetadataArtefact | MetadataPartitions | MetadataSQLQuery | MetadataSQLColumn .
    MetadataCalls = "+" "calls" Ref ("," Ref)* .
    Ref = (<ident> ".")? <ident> ("<" Type ("," Type)* ">")? .
    MetadataConfig = "+" "config" Ref ("," Ref)* .
    MetadataIngress = "+" "ingress" "http"? ("GET" | "POST" | "PUT" | "DELETE") ("/" IngressPathComponent)+ .
    IngressPathComponent = IngressPathLiteral | IngressPathParameter .
    IngressPathLiteral = ~(<whitespace> | "/" | "{" | "}")+ .
    IngressPathParameter = "{" <ident> "}" .
    MetadataCronJob = "+" "cron" <whitespace> (" " | ~<eol>)+ .
    MetadataDatabases = "+" "database" "calls" Ref ("," Ref)* .
    MetadataAlias = "+" "alias" <ident> <string> .
    MetadataRetry = "+" "retry" (<number> <whitespace>)? (<number> (?! <whitespace>) <ident>)? (<number> (?! <whitespace>) <ident>)? ("catch" Ref)? .
    MetadataSecrets = "+" "secrets" Ref ("," Ref)* .
    MetadataSubscriber = "+" "subscribe" Ref "from" "=" ("beginning" | "latest") "deadletter"? .
    MetadataTypeMap = "+" "typemap" ("go" | "kotlin" | "java") <string> .
    MetadataEncoding = "+" "encoding" "json" "lenient"? .
    MetadataPublisher = "+" "publish" Ref ("," Ref)* .
    MetadataSQLMigration = "+" "migration" <whitespace> "sha256" ":" <sha256> .
    MetadataArtefact = "+" "artefact" <whitespace> <string> <string> "executable"? .
    MetadataPartitions = "+" "partitions" <number> .
    MetadataSQLQuery = "+" "sql" <whitespace> "query" <whitespace> ":" <ident> <whitespace> <string> .
    MetadataSQLColumn = "+" "sql" <whitespace> "column" <whitespace> <string> "." <string> .
    Decl = Data | Verb | Database | Enum | TypeAlias | Config | Secret | Topic .
    Data = <comment>* "export"? "data" <ident> ("<" TypeParameter ("," TypeParameter)* ">")? "{" Field* "}" Metadata* .
    TypeParameter = <ident> .
    Field = <comment>* <ident> Type Metadata* .
    Verb = <comment>* "export"? "verb" <ident> "(" Type ")" Type Metadata* .
    Database = <comment>* "database" ("postgres" | "mysql") <ident> Metadata* .
    Enum = <comment>* "export"? "enum" <ident> (":" Type)? "{" EnumVariant* "}" .
    EnumVariant = <comment>* <ident> (("=" Value) | Value)! .
    Value = StringValue | IntValue | TypeValue .
    StringValue = <string> .
    IntValue = <number> .
    TypeValue = Type .
    TypeAlias = <comment>* "export"? "typealias" <ident> Type Metadata* .
    Config = <comment>* "config" <ident> Type .
    Secret = <comment>* "secret" <ident> Type .
    Topic = <comment>* "export"? "topic" <ident> Type Metadata* .
    ```

--- cmd/ftl/cmd_schema.go
+++ cmd/ftl/cmd_schema.go
@@ -4,4 +4,5 @@ type schemaCmd struct {
 	Get      getSchemaCmd      `default:"" cmd:"" help:"Retrieve the cluster FTL schema."`
 	Diff     schemaDiffCmd     `cmd:"" help:"Print any schema differences between this cluster and another cluster. Returns an exit code of 1 if there are differences."`
 	Generate schemaGenerateCmd `cmd:"" help:"Stream the schema from the cluster and generate files from the template."`
+	EBNF     schemaEBNFCmd     `cmd:"" help:"Print the EBNF grammar for the FTL schema."`
 }
