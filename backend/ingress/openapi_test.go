635939b45deff4cfd35248da8a9d8820540831e7
Alec Thomas <aat@block.xyz>
2025-02-28 19:59:48 +1100

    feat: add OpenAPI spec generator (#4736)
    This function translates an FTL schema with ingress annotations into an
    OpenAPI spec. This isn't wired up anywhere yet, will do so in followup.
    
    Fixed #920
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ backend/ingress/openapi_test.go
@@ -0,0 +1,215 @@
+package ingress
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/block/ftl/common/schema"
+)
+
+func TestOpenAPI(t *testing.T) {
+	s, err := schema.ParseString("ingress", `
+		module http {
+		  	export data ApiError {
+				message String +alias json "message"
+			}
+				
+			export data GetPathParams {
+				name String +alias json "name"
+			}
+			
+			export data GetQueryParams {
+				age Int? +alias json "age"
+			} 
+
+			export data GetResponse {
+				name String +alias json "name"
+				age Int? +alias json "age"
+			}
+			
+			export data PostRequest {
+				name String +alias json "name"
+				age Int +alias json "age"
+			}
+
+			export data PostResponse {
+				name String +alias json "name"
+				age Int +alias json "age"
+			}
+
+			// Example usage of path and query params
+			// curl http://localhost:8891/get/wicket?age=10
+			export verb get(builtin.HttpRequest<Unit, http.GetPathParams, http.GetQueryParams>) builtin.HttpResponse<http.GetResponse, http.ApiError>
+				+ingress http GET /get/{name}
+
+			// Example POST request with a JSON body
+			// curl -X POST http://localhost:8891/post -d '{"name": "wicket", "age": 10}'
+			export verb post(builtin.HttpRequest<http.PostRequest, Unit, Unit>) builtin.HttpResponse<http.PostResponse, http.ApiError> 
+				+ingress http POST /post
+		}
+	`)
+	assert.NoError(t, err)
+
+	// Convert the schema to OpenAPI
+	swagger := SchemaToOpenAPI(s)
+
+	// Convert the OpenAPI spec to JSON
+	actualJSON, err := json.MarshalIndent(swagger, "", "  ")
+	assert.NoError(t, err)
+
+	// Define the expected JSON
+	expectedJSON := `{
+  "swagger": "2.0",
+  "info": {
+    "description": "API generated from FTL schema",
+    "title": "FTL API",
+    "version": "1.0.0"
+  },
+  "paths": {
+    "/get/{name}": {
+      "get": {
+        "description": "Example usage of path and query params\ncurl http://localhost:8891/get/wicket?age=10",
+        "consumes": [
+          "application/json"
+        ],
+        "produces": [
+          "application/json"
+        ],
+        "operationId": "http.get",
+        "parameters": [
+          {
+            "name": "name",
+            "in": "path",
+            "required": true,
+            "schema": {
+              "type": "string"
+            }
+          },
+          {
+            "name": "age",
+            "in": "query",
+            "schema": {
+              "type": "integer",
+              "format": "int64"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Successful response",
+            "schema": {
+              "$ref": "#/definitions/http.GetResponse"
+            }
+          },
+          "400": {
+            "description": "Error response",
+            "schema": {
+              "$ref": "#/definitions/http.ApiError"
+            }
+          }
+        }
+      }
+    },
+    "/post": {
+      "post": {
+        "description": "Example POST request with a JSON body\ncurl -X POST http://localhost:8891/post -d '{\"name\": \"wicket\", \"age\": 10}'",
+        "consumes": [
+          "application/json"
+        ],
+        "produces": [
+          "application/json"
+        ],
+        "operationId": "http.post",
+        "parameters": [
+          {
+            "description": "Request body",
+            "name": "body",
+            "in": "body",
+            "required": true,
+            "schema": {
+              "$ref": "#/definitions/http.PostRequest"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Successful response",
+            "schema": {
+              "$ref": "#/definitions/http.PostResponse"
+            }
+          },
+          "400": {
+            "description": "Error response",
+            "schema": {
+              "$ref": "#/definitions/http.ApiError"
+            }
+          }
+        }
+      }
+    }
+  },
+  "definitions": {
+    "http.ApiError": {
+      "type": "object",
+      "required": [
+        "message"
+      ],
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    },
+    "http.GetResponse": {
+      "type": "object",
+      "required": [
+        "name"
+      ],
+      "properties": {
+        "age": {
+          "type": "integer",
+          "format": "int64"
+        },
+        "name": {
+          "type": "string"
+        }
+      }
+    },
+    "http.PostRequest": {
+      "type": "object",
+      "required": [
+        "name",
+        "age"
+      ],
+      "properties": {
+        "age": {
+          "type": "integer",
+          "format": "int64"
+        },
+        "name": {
+          "type": "string"
+        }
+      }
+    },
+    "http.PostResponse": {
+      "type": "object",
+      "required": [
+        "name",
+        "age"
+      ],
+      "properties": {
+        "age": {
+          "type": "integer",
+          "format": "int64"
+        },
+        "name": {
+          "type": "string"
+        }
+      }
+    }
+  }
+}`
+
+	assert.Equal(t, string(actualJSON), expectedJSON)
+}
--- go.mod
+++ go.mod
@@ -33,6 +33,7 @@ require (
 	github.com/docker/docker v27.5.1+incompatible
 	github.com/docker/go-connections v0.5.0
 	github.com/go-logr/logr v1.4.2
+	github.com/go-openapi/spec v0.21.0
 	github.com/go-sql-driver/mysql v1.9.0
 	github.com/go-viper/mapstructure/v2 v2.2.1
 	github.com/google/go-cmp v0.7.0
@@ -132,7 +133,7 @@ require (
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/getsentry/sentry-go v0.12.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
-	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
