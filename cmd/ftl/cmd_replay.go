a76724c31341777ae6ee6baed00d344380611cb8
Matt Toohey <mtoohey@block.xyz>
2025-03-12 10:33:17 +1100

    feat: `ftl call -v` prints out more info (just request key for now) (#4830)
    - It can be helpful to know the request key to then understand what
    happened as part of the call (via the timeline)
        - Goose will make use of this
    - We will probably want more info in the future so i made the flag not
    request key specific
    - Verb router now includes the request key in the call response it
    creates

--- cmd/ftl/cmd_replay.go
+++ cmd/ftl/cmd_replay.go
@@ -20,8 +20,9 @@ import (
 )
 
 type replayCmd struct {
-	Wait time.Duration  `short:"w" help:"Wait up to this elapsed time for the FTL cluster to become available." default:"1m"`
-	Verb reflection.Ref `arg:"" required:"" help:"Full path of Verb to call." predictor:"verbs"`
+	Wait    time.Duration  `short:"w" help:"Wait up to this elapsed time for the FTL cluster to become available." default:"1m"`
+	Verb    reflection.Ref `arg:"" required:"" help:"Full path of Verb to call." predictor:"verbs"`
+	Verbose bool           `flag:"" short:"v" help:"Print verbose information."`
 }
 
 func (c *replayCmd) Run(
@@ -98,5 +99,5 @@ func (c *replayCmd) Run(
 	logger.Infof("Calling %s with body:", c.Verb)
 	status.PrintJSON(ctx, []byte(requestJSON))
 	logger.Infof("Response:")
-	return callVerb(ctx, verbClient, eventSource, c.Verb, []byte(requestJSON))
+	return callVerb(ctx, verbClient, eventSource, c.Verb, []byte(requestJSON), c.Verbose)
 }
