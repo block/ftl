485d5376052b387813c9d78993fda1f4e6955d10
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-03-13 07:55:28 +1100

    fix: limit log files to 10mb (#4819)
    fixes: #4815

--- internal/log/debug.go
+++ internal/log/debug.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 
 	"github.com/alecthomas/types/optional"
+
+	"github.com/block/ftl-golang-tools/go/ssa/interp/testdata/src/sync"
 )
 
 const logFileName = "log.json"
@@ -18,25 +20,62 @@ func SetupDebugFileLogging(ctx context.Context, root string, maxLogs int) {
 		logger.Errorf(err, "Could not create log directory %s", logDir)
 		return
 	}
-	if err := rotateLogs(logDir, maxLogs); err != nil {
-		logger.Errorf(err, "Could not rotate logs")
-		return
-	}
-	logFile, err := os.Create(filepath.Join(logDir, logFileName))
-	if err != nil {
-		logger.Errorf(err, "Could not create log file")
-		return
-	}
+	sink := &logRotatingSink{maxLogs: maxLogs, logDir: logDir}
 	context.AfterFunc(ctx, func() {
-		if logFile != nil {
-			logFile.Close() // #nolint: errcheck
+		sink.lock.Lock()
+		defer sink.lock.Unlock()
+		if sink.file != nil {
+			sink.file.Close() // #nolint: errcheck
 		}
 	})
-	sink := newJSONSink(logFile)
-	logger.debugLogger = optional.Some(devModeDebugLogger{delegate: New(Debug, sink), file: logFile.Name()})
+	logger.debugLogger = optional.Some(devModeDebugLogger{delegate: New(Debug, sink), sink: sink})
 }
 
-func rotateLogs(workingDir string, maxLogs int) error {
+var _ Sink = (*logRotatingSink)(nil)
+
+type logRotatingSink struct {
+	maxLogs int
+	count   int
+	lock    sync.Mutex
+	file    *os.File
+	logDir  string
+	sink    Sink
+}
+
+func (r *logRotatingSink) Log(entry Entry) error {
+	var err error
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	r.count++
+	if r.file == nil {
+		r.file, err = rotateLogs(r.logDir, r.maxLogs)
+		r.sink = newJSONSink(r.file)
+		if err != nil {
+			return fmt.Errorf("could not rotate logs: %w", err)
+		}
+	} else if r.count%100 == 0 {
+		// We only check the size every hundred logs to avoid a performance hit
+		size, err := r.file.Stat()
+		if err != nil {
+			return fmt.Errorf("could not stat log file: %w", err)
+		}
+		if size.Size() > 1024*1024*10 {
+			r.file.Close() // #nolint: errcheck
+			r.file, err = rotateLogs(r.logDir, r.maxLogs)
+			r.sink = newJSONSink(r.file)
+			if err != nil {
+				return fmt.Errorf("could not rotate logs: %w", err)
+			}
+		}
+	}
+	err = r.sink.Log(entry)
+	if err != nil {
+		return fmt.Errorf("could not write log entry: %w", err)
+	}
+	return nil
+}
+
+func rotateLogs(workingDir string, maxLogs int) (*os.File, error) {
 	// Remove the oldest log file if it exists
 	oldestLog := filepath.Join(workingDir, fmt.Sprintf("log.%d.json", maxLogs))
 	if _, err := os.Stat(oldestLog); err == nil {
@@ -50,7 +89,7 @@ func rotateLogs(workingDir string, maxLogs int) error {
 		if _, err := os.Stat(oldName); err == nil {
 			err := os.Rename(oldName, newName)
 			if err != nil {
-				return fmt.Errorf("could not rename log file %s to %s: %w", oldName, newName, err)
+				return nil, fmt.Errorf("could not rename log file %s to %s: %w", oldName, newName, err)
 			}
 		}
 	}
@@ -60,11 +99,16 @@ func rotateLogs(workingDir string, maxLogs int) error {
 	if _, err := os.Stat(currentLog); err == nil {
 		err := os.Rename(currentLog, filepath.Join(workingDir, "log.1.json"))
 		if err != nil {
-			return fmt.Errorf("could not rename log file %s: %w", currentLog, err)
+			return nil, fmt.Errorf("could not rename log file %s: %w", currentLog, err)
 		}
 	}
 
-	return nil
+	logFile, err := os.Create(filepath.Join(workingDir, logFileName))
+	if err != nil {
+		return nil, fmt.Errorf("could not create log file: %w", err)
+	}
+
+	return logFile, nil
 }
 
 // ReplayLogs replays the debug logs to the delegate logger
@@ -72,7 +116,7 @@ func ReplayLogs(ctx context.Context) {
 	logger := FromContext(ctx)
 	if t, ok := logger.debugLogger.Get(); ok {
 		go func() {
-			fileName := t.file
+			fileName := t.sink.file.Name()
 			// Make a copy of the logger and remove the debug logger
 			// This is so we don't re-write all the messages to the log file
 			logCopy := *logger
@@ -92,5 +136,5 @@ func ReplayLogs(ctx context.Context) {
 
 type devModeDebugLogger struct {
 	delegate *Logger
-	file     string
+	sink     *logRotatingSink
 }
