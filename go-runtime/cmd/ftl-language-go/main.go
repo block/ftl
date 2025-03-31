db9918e4230c472b2145dac0de1ec3556ce8163e
Alec Thomas <aat@block.xyz>
2025-03-31 20:12:36 +1100

    refactor: simplify plugin command invocation

--- go-runtime/cmd/ftl-language-go/main.go
+++ go-runtime/cmd/ftl-language-go/main.go
@@ -4,24 +4,21 @@ import (
 	"context"
 	"os"
 
-	"connectrpc.com/connect"
 	"github.com/alecthomas/kong"
 
 	"github.com/block/ftl"
-	languagepb "github.com/block/ftl/backend/protos/xyz/block/ftl/language/v1"
 	"github.com/block/ftl/backend/protos/xyz/block/ftl/language/v1/languagepbconnect"
 	"github.com/block/ftl/common/plugin"
 	"github.com/block/ftl/go-runtime/goplugin"
 	"github.com/block/ftl/internal/clirpc"
+	"github.com/block/ftl/internal/log"
 )
 
 var cli struct {
+	Logging log.Config       `embed:"" prefix:"log-"`
 	Version kong.VersionFlag `help:"Show version."`
-	Serve   serve            `cmd:"" default:"1" help:"Run the service."`
-
-	GetNewModuleFlags       GetNewModuleFlagsCmd       `cmd:"" help:"Get language specific flags for a new module."`
-	NewModule               NewModuleCmd               `cmd:"" help:"Create a new module."`
-	GetModuleConfigDefaults GetModuleConfigDefaultsCmd `cmd:"" help:"Get the default config for a module."`
+	Name    string           `env:"FTL_NAME" help:"Name of plugin as provided by plugin host."`
+	Command string           `arg:"" optional:"" help:"Command to run synchronously. Request is passed as proto-encoded bytes on stdin, and response returned on stdout."`
 }
 
 type serve struct {
@@ -33,64 +30,25 @@ func main() {
 		kong.UsageOnError(),
 		kong.Vars{"version": ftl.FormattedVersion},
 	)
+	if cli.Command == "" {
+		plugin.Start(context.Background(),
+			cli.Name,
+			createService,
+			languagepbconnect.LanguageServiceName,
+			languagepbconnect.NewLanguageServiceHandler)
+	} else {
+		ctx := log.ContextWithLogger(context.Background(), log.Configure(os.Stderr, cli.Logging))
+		err := clirpc.Invoke(ctx, cli.Command, os.Stdin, os.Stdout)
+		kctx.FatalIfErrorf(err)
+	}
 	kctx.FatalIfErrorf(kctx.Run())
 }
 
 func (s serve) Run() error {
-	plugin.Start(context.Background(),
-		os.Getenv("FTL_NAME"),
-		createService,
-		languagepbconnect.LanguageServiceName,
-		languagepbconnect.NewLanguageServiceHandler)
 	return nil
 }
 
 func createService(ctx context.Context, config any) (context.Context, *goplugin.Service, error) {
 	svc := goplugin.New()
 	return ctx, svc, nil
-}
-
-type GetNewModuleFlagsCmd struct {
-	clirpc.Command
-}
-
-func (cmd GetNewModuleFlagsCmd) Run() error {
-	cmdSvc := goplugin.CmdService{}
-	return clirpc.Run(cmd.Command, func(ctx context.Context, req *languagepb.GetNewModuleFlagsRequest) (*languagepb.GetNewModuleFlagsResponse, error) {
-		resp, err := cmdSvc.GetNewModuleFlags(ctx, connect.NewRequest(req))
-		if err != nil {
-			return nil, err //nolint:wrapcheck
-		}
-		return resp.Msg, nil
-	})
-}
-
-type NewModuleCmd struct {
-	clirpc.Command
-}
-
-func (cmd NewModuleCmd) Run() error {
-	cmdSvc := goplugin.CmdService{}
-	return clirpc.Run(cmd.Command, func(ctx context.Context, req *languagepb.NewModuleRequest) (*languagepb.NewModuleResponse, error) {
-		resp, err := cmdSvc.NewModule(ctx, connect.NewRequest(req))
-		if err != nil {
-			return nil, err //nolint:wrapcheck
-		}
-		return resp.Msg, nil
-	})
-}
-
-type GetModuleConfigDefaultsCmd struct {
-	clirpc.Command
-}
-
-func (cmd GetModuleConfigDefaultsCmd) Run() error {
-	cmdSvc := goplugin.CmdService{}
-	return clirpc.Run(cmd.Command, func(ctx context.Context, req *languagepb.GetModuleConfigDefaultsRequest) (*languagepb.GetModuleConfigDefaultsResponse, error) {
-		resp, err := cmdSvc.GetModuleConfigDefaults(ctx, connect.NewRequest(req))
-		if err != nil {
-			return nil, err //nolint:wrapcheck
-		}
-		return resp.Msg, nil
-	})
 }
--- internal/clirpc/clirpc.go
+++ internal/clirpc/clirpc.go
@@ -5,11 +5,15 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"reflect"
 
 	"google.golang.org/protobuf/proto"
 
+	"github.com/block/ftl/backend/protos/xyz/block/ftl/language/v1/languagepbconnect"
+	"github.com/block/ftl/go-runtime/goplugin"
 	"github.com/block/ftl/internal/log"
 )
 
@@ -39,5 +43,23 @@ func Run[Req, Resp proto.Message](command Command, handler CommandHandler[Req, R
 		return fmt.Errorf("failed to marshal response: %w", err)
 	}
 	os.Stdout.Write(bytes)
+	return nil
+}
+
+func Invoke(ctx context.Context, command string, req io.Reader, resp io.Writer) error {
+	path, handler := languagepbconnect.NewLanguageCommandServiceHandler(goplugin.CmdService{})
+	hreq, err := http.NewRequestWithContext(ctx, "POST", "http://localhost"+path+command, req)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, hreq)
+	if w.Code != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d: %s", w.Code, w.Body.String())
+	}
+	_, err = io.Copy(resp, w.Body)
+	if err != nil {
+		return fmt.Errorf("failed to write response: %w", err)
+	}
 	return nil
 }
