45e38d6b50bb2feb0f441c09605e0ee0c6b8ef6b
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-04-11 15:34:17 +1000

    fix: reduce dev mode start time (#5119)

--- jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/StaticConfigSource.java
+++ jvm-runtime/ftl-runtime/common/deployment/src/main/java/xyz/block/ftl/deployment/StaticConfigSource.java
@@ -1,42 +1,28 @@
 package xyz.block.ftl.deployment;
 
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
 public class StaticConfigSource implements ConfigSource {
 
-    public static final String QUARKUS_BANNER_ENABLED = "quarkus.banner.enabled";
-    final static String OTEL_METRICS_ENABLED = "quarkus.otel.metrics.enabled";
-    final static String DEV_SERVICES_ENABLED = "quarkus.devservices.enabled";
-    final static String LOG_FORMAT = "quarkus.log.console.format";
-    final static String LIVE_RELOAD_ENABLED = "quarkus.live-reload.enabled";
+    static final Map<String, String> CONFIG = Map.of(
+            "quarkus.banner.enabled", "false",
+            "quarkus.otel.metrics.enabled", "true",
+            "quarkus.devservices.enabled", "false",
+            "quarkus.log.console.format", "%p %m",
+            "quarkus.live-reload.enabled", "false",
+            "quarkus.console.enabled", "false");
 
     @Override
     public Set<String> getPropertyNames() {
-        return Set.of(QUARKUS_BANNER_ENABLED);
+        return CONFIG.keySet();
     }
 
     @Override
     public String getValue(String propertyName) {
-        switch (propertyName) {
-            case (QUARKUS_BANNER_ENABLED) -> {
-                return "false";
-            }
-            case OTEL_METRICS_ENABLED -> {
-                return "true";
-            }
-            case DEV_SERVICES_ENABLED -> {
-                return "false";
-            }
-            case LOG_FORMAT -> {
-                return "%p %m";
-            }
-            case LIVE_RELOAD_ENABLED -> {
-                return "false";
-            }
-        }
-        return null;
+        return CONFIG.get(propertyName);
     }
 
     @Override
