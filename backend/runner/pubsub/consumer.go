af62a9c9574e647bfa1c1a97ae1ce4586c8db029
Matt Toohey <contact@matttoohey.com>
2025-04-17 11:19:17 +1000

    do not keep retryng if context gets cancelled

--- backend/runner/pubsub/consumer.go
+++ backend/runner/pubsub/consumer.go
@@ -331,13 +331,6 @@ func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 				if err == nil {
 					break
 				}
-				var connectErr *connect.Error
-				if errors.As(err, &connectErr) {
-					// Connection error, do not count as an attempt
-					// This can happen when a runner is shutting down. This should never mark the message as consumed.
-					time.Sleep(time.Second)
-					continue
-				}
 				select {
 				case <-ctx.Done():
 					// Do not commit the message if we did not succeed and the context is done.
@@ -345,6 +338,13 @@ func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 					logger.Errorf(err, "Failed to consume message from %v[%v,%v]", c.verb.Name, msg.Partition, msg.Offset)
 					return nil
 				default:
+				}
+				var connectErr *connect.Error
+				if errors.As(err, &connectErr) {
+					// Connection error, do not count as an attempt
+					// This can happen when a runner is shutting down. This should never mark the message as consumed.
+					time.Sleep(time.Second)
+					continue
 				}
 				if remainingRetries == 0 {
 					logger.Errorf(err, "Failed to consume message from %v[%v,%v]", c.verb.Name, msg.Partition, msg.Offset)
