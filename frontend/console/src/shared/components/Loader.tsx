784dbca5b29498c7786703438ae2ec8a7f80c629
Wes <wesbillman@users.noreply.github.com>
2025-05-09 08:18:48 -0700

    feat: add a loading spinner when calling ftl verbs

--- frontend/console/src/shared/components/Loader.tsx
+++ frontend/console/src/shared/components/Loader.tsx
@@ -1,8 +1,8 @@
-export const Loader = () => {
+export const Loader = ({ className = 'w-8 h-8' }: { className?: string }) => {
   return (
     <svg
       aria-hidden='true'
-      className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+      className={`${className} text-gray-200 animate-spin dark:text-gray-600 fill-indigo-600`}
       viewBox='0 0 100 101'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
