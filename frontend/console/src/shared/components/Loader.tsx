eb97889f3f3d6152f62964f3b6ae37afb6e88282
Wes <wesbillman@users.noreply.github.com>
2025-05-09 08:40:25 -0700

    fix: minor darkmode fix

--- frontend/console/src/shared/components/Loader.tsx
+++ frontend/console/src/shared/components/Loader.tsx
@@ -2,7 +2,7 @@ export const Loader = ({ className = 'w-8 h-8' }: { className?: string }) => {
   return (
     <svg
       aria-hidden='true'
-      className={`${className} text-gray-200 animate-spin dark:text-gray-600 fill-indigo-600`}
+      className={`${className} text-gray-200 dark:text-gray-300 animate-spin fill-indigo-600 dark:fill-indigo-400`}
       viewBox='0 0 100 101'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
