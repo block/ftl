f84d05bad6a4a45b7499d5bb3c41d3567a325810
Juho Makinen <juho@squareup.com>
2025-05-19 15:35:18 +1000

    chore: rust test for names sql queries

--- sqlc-gen-ftl/test/sqlc_gen_ftl_test.rs
+++ sqlc-gen-ftl/test/sqlc_gen_ftl_test.rs
@@ -35,6 +35,7 @@ fn get_test_queries(engine: &str) -> Vec<String> {
             "INSERT INTO all_types ( big_int, small_int, some_decimal, some_numeric, some_float, some_double, some_varchar, some_text, some_char, nullable_text, some_bool, nullable_bool, some_date, some_time, some_timestamp, some_blob, some_json ) VALUES ( @big_int, @small_int, @some_decimal, @some_numeric, @some_float, @some_double, @some_varchar, @some_text, @some_char, @nullable_text, @some_bool, @nullable_bool, @some_date, @some_time, @some_timestamp, @some_blob, @some_json )".to_string(),
             "INSERT INTO requests (data) VALUES (CONCAT(LOWER(?), LOWER(?)))".to_string(),
             "SELECT id, data FROM requests WHERE id in (/*SLICE:request_ids*/?)".to_string(),
+            "SELECT * FROM authors WHERE id = CAST(sqlc.arg('named_id') AS UNSIGNED) AND hometown = ?;".to_string(),
         ],
         "postgresql" => vec![
             "SELECT id, big_int, small_int, some_decimal, some_numeric, some_float, some_double, some_varchar, some_text, some_char, nullable_text, some_bool, nullable_bool, some_date, some_time, some_timestamp, some_blob, some_json FROM all_types WHERE id = $1".to_string(),
@@ -45,6 +46,7 @@ fn get_test_queries(engine: &str) -> Vec<String> {
             "INSERT INTO all_types ( big_int, small_int, some_decimal, some_numeric, some_float, some_double, some_varchar, some_text, some_char, nullable_text, some_bool, nullable_bool, some_date, some_time, some_timestamp, some_blob, some_json ) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17 )".to_string(),
             "INSERT INTO requests (data) VALUES (CONCAT(LOWER($1), LOWER($2)))".to_string(),
             "SELECT id, data FROM requests WHERE id = ANY($1::int[])".to_string(),
+            "SELECT * FROM authors WHERE id = sqlc.arg('named_id')::int AND hometown = ?;".to_string(),
         ],
         _ => vec![],
     }
