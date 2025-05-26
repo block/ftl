7fe1fb9497ed6eeee8ae7b380cdebb2118eb3df0
Stuart Douglas <sdouglas@block.xyz>
2025-05-26 12:26:06 +1000

    fix: make sure request id is propagated on ingress

--- jvm-runtime/ftl-runtime/common/runtime/src/main/java/xyz/block/ftl/runtime/CurrentRequestClientInterceptor.java
+++ jvm-runtime/ftl-runtime/common/runtime/src/main/java/xyz/block/ftl/runtime/CurrentRequestClientInterceptor.java
@@ -3,6 +3,8 @@ package xyz.block.ftl.runtime;
 import java.util.Locale;
 import java.util.Set;
 
+import org.jboss.resteasy.reactive.server.core.CurrentRequestManager;
+
 import io.grpc.CallOptions;
 import io.grpc.Channel;
 import io.grpc.ClientCall;
@@ -13,8 +15,10 @@ import io.grpc.MethodDescriptor;
 
 public class CurrentRequestClientInterceptor implements io.grpc.ClientInterceptor {
 
-    private static final Set<String> PROTOCOL_HEADERS = Set.of("content-type", "content-length", "user-agent", "host",
+    public static final Set<String> PROTOCOL_HEADERS = Set.of("content-type", "content-length", "user-agent", "host",
             "transfer-encoding", "te", "trailer", "accept-encoding", "accept", "accept-language", "connection");
+    public static final Metadata.Key<String> FTL_REQUEST_ID = Metadata.Key.of("ftl-request-id",
+            Metadata.ASCII_STRING_MARSHALLER);
 
     @Override
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method,
@@ -26,6 +30,15 @@ public class CurrentRequestClientInterceptor implements io.grpc.ClientIntercepto
             public void start(Listener<RespT> responseListener, Metadata headers) {
 
                 Metadata current = CurrentRequestServerInterceptor.METADATA.get();
+                if (current == null) {
+                    var c = CurrentRequestManager.get();
+                    if (c != null) {
+                        String req = c.getRequestHeaders().getHeaderString("ftl-request-id");
+                        if (req != null) {
+                            headers.put(FTL_REQUEST_ID, req);
+                        }
+                    }
+                }
                 if (current != null) {
                     for (var entry : current.keys()) {
                         if (PROTOCOL_HEADERS.contains(entry.toLowerCase(Locale.ENGLISH))) {
