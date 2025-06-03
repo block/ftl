a2c1d83feea2a8e369868daf7f19aac5ae8311ce
Stuart Douglas <sdouglas@block.xyz>
2025-06-04 07:27:05 +1000

    fix

--- internal/config/kubeconfig/kube_secret.go
+++ internal/config/kubeconfig/kube_secret.go
@@ -13,23 +13,23 @@ import (
 	"github.com/block/ftl/internal/kube"
 )
 
-const KubeConfigMapProviderKind config.ProviderKind = "kube-config-map"
+const KubeSecretProviderKind config.ProviderKind = "kube-secret"
 
-var _ config.Provider[config.Configuration] = &KubeConfigMapProvider{}
+var _ config.Provider[config.Secrets] = &KubeSecretProvider{}
 
-type KubeConfigMapProvider struct {
+type KubeSecretProvider struct {
 	client *kubernetes.Clientset
 	mapper kube.NamespaceMapper
 	realm  string
 }
 
 // Close implements Provider.
-func (k *KubeConfigMapProvider) Close(ctx context.Context) error {
+func (k *KubeSecretProvider) Close(ctx context.Context) error {
 	return nil
 }
 
 // Delete implements Provider.
-func (k *KubeConfigMapProvider) Delete(ctx context.Context, ref config.Ref) error {
+func (k *KubeSecretProvider) Delete(ctx context.Context, ref config.Ref) error {
 	if module, ok := ref.Module.Get(); ok {
 		conf, err := k.load(ctx, module)
 		if err != nil {
@@ -45,43 +45,43 @@ func (k *KubeConfigMapProvider) Delete(ctx context.Context, ref config.Ref) erro
 }
 
 // Key implements Provider.
-func (k *KubeConfigMapProvider) Key() config.ProviderKey {
-	return config.NewProviderKey(KubeConfigMapProviderKind)
+func (k *KubeSecretProvider) Key() config.ProviderKey {
+	return config.NewProviderKey(KubeSecretProviderKind)
 }
 
 // List implements Provider.
-func (k *KubeConfigMapProvider) List(ctx context.Context, withValues bool, forModule optional.Option[string]) ([]config.Value, error) {
-	// Not implementd yet
+func (k *KubeSecretProvider) List(ctx context.Context, withValues bool, forModule optional.Option[string]) ([]config.Value, error) {
+	// Not implemented yet
 	return []config.Value{}, nil
 }
 
 // Load implements Provider.
-func (k *KubeConfigMapProvider) Load(ctx context.Context, ref config.Ref) ([]byte, error) {
+func (k *KubeSecretProvider) Load(ctx context.Context, ref config.Ref) ([]byte, error) {
 	if module, ok := ref.Module.Get(); ok {
 		ns := k.mapper(module, k.realm)
-		cm, err := k.client.CoreV1().ConfigMaps(ns).Get(ctx, kube.ConfigMapName(module), v1.GetOptions{})
+		cm, err := k.client.CoreV1().Secrets(ns).Get(ctx, kube.SecretName(module), v1.GetOptions{})
 		if err != nil {
 			if k8serrors.IsNotFound(err) {
 				return nil, nil
 			}
-			return nil, errors.Wrap(err, "failed to get ConfigMap")
+			return nil, errors.Wrap(err, "failed to get Secret")
 		}
 		val := cm.Data[ref.Name]
-		if val == "" {
+		if len(val) == 0 {
 			return nil, nil
 		}
-		return []byte(val), nil
+		return val, nil
 	}
 	return nil, nil
 }
 
 // Role implements Provider.
-func (k *KubeConfigMapProvider) Role() config.Configuration {
-	return config.Configuration{}
+func (k *KubeSecretProvider) Role() config.Secrets {
+	return config.Secrets{}
 }
 
 // Store implements Provider.
-func (k *KubeConfigMapProvider) Store(ctx context.Context, ref config.Ref, value []byte) error {
+func (k *KubeSecretProvider) Store(ctx context.Context, ref config.Ref, value []byte) error {
 
 	if module, ok := ref.Module.Get(); ok {
 		conf, err := k.load(ctx, module)
@@ -97,18 +97,18 @@ func (k *KubeConfigMapProvider) Store(ctx context.Context, ref config.Ref, value
 	return errors.Errorf("unable to set global config")
 }
 
-func NewKubeConfigProvider(client *kubernetes.Clientset, mapper kube.NamespaceMapper, realm string) *KubeConfigMapProvider {
-	return &KubeConfigMapProvider{client: client, mapper: mapper, realm: realm}
+func NewKubeSecretProvider(client *kubernetes.Clientset, mapper kube.NamespaceMapper, realm string) *KubeSecretProvider {
+	return &KubeSecretProvider{client: client, mapper: mapper, realm: realm}
 }
 
-func (k *KubeConfigMapProvider) load(ctx context.Context, module string) (map[string][]byte, error) {
+func (k *KubeSecretProvider) load(ctx context.Context, module string) (map[string][]byte, error) {
 	ns := k.mapper(module, k.realm)
-	cm, err := k.client.CoreV1().ConfigMaps(ns).Get(ctx, kube.ConfigMapName(module), v1.GetOptions{})
+	cm, err := k.client.CoreV1().Secrets(ns).Get(ctx, kube.SecretName(module), v1.GetOptions{})
 	if err != nil {
 		if k8serrors.IsNotFound(err) {
 			return map[string][]byte{}, nil
 		}
-		return nil, errors.Wrap(err, "failed to get ConfigMap")
+		return nil, errors.Wrap(err, "failed to get Secret")
 	}
 	serialisable := cm.Data
 	if cm.Data == nil {
@@ -117,39 +117,39 @@ func (k *KubeConfigMapProvider) load(ctx context.Context, module string) (map[st
 
 	out := map[string][]byte{}
 	for refStr, keyStr := range serialisable {
-		out[refStr] = []byte(keyStr)
+		out[refStr] = keyStr
 	}
 	return out, nil
 }
 
-func (k *KubeConfigMapProvider) save(ctx context.Context, module string, data map[string][]byte) error {
-	serialisable := map[string]string{}
-	for ref, key := range data {
-		serialisable[ref] = string(key)
+func (k *KubeSecretProvider) save(ctx context.Context, module string, data map[string][]byte) error {
+	serialisable := map[string][]byte{}
+	for ref, val := range data {
+		serialisable[ref] = val
 	}
 	ns := k.mapper(module, k.realm)
 	err := kube.EnsureNamespace(ctx, k.client, k.mapper(module, k.realm), k.realm)
 	if err != nil {
 		return errors.Wrapf(err, "unable to create namespace")
 	}
-	cm, err := k.client.CoreV1().ConfigMaps(ns).Get(ctx, kube.ConfigMapName(module), v1.GetOptions{})
+	cm, err := k.client.CoreV1().Secrets(ns).Get(ctx, kube.SecretName(module), v1.GetOptions{})
 	if err != nil {
 		if !k8serrors.IsNotFound(err) {
-			return errors.Wrap(err, "failed to get ConfigMap")
+			return errors.Wrap(err, "failed to get Secret")
 		}
-		cm.Name = kube.ConfigMapName(module)
+		cm.Name = kube.SecretName(module)
 		cm.Namespace = ns
 		cm.Labels = map[string]string{"app.kubernetes.io/managed-by": "ftl"}
-		cm, err = k.client.CoreV1().ConfigMaps(ns).Create(ctx, cm, v1.CreateOptions{})
+		cm, err = k.client.CoreV1().Secrets(ns).Create(ctx, cm, v1.CreateOptions{})
 		if err != nil {
-			return errors.Wrap(err, "failed to create ConfigMap")
+			return errors.Wrap(err, "failed to create Secret")
 		}
 	}
 	cm.Data = serialisable
 
-	_, err = k.client.CoreV1().ConfigMaps(ns).Update(ctx, cm, v1.UpdateOptions{})
+	_, err = k.client.CoreV1().Secrets(ns).Update(ctx, cm, v1.UpdateOptions{})
 	if err != nil {
-		return errors.Wrap(err, "failed to update ConfigMap")
+		return errors.Wrap(err, "failed to update Secret")
 	}
 	return nil
 }
